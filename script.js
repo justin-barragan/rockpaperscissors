06606dc26a397e71c17687c98d36c3d7d2c01252
Justin Barragan <justinbarragan1@gmail.com>
2022-01-29 21:41:14 -0800

    Added an input format function

--- script.js
+++ script.js
@@ -12,17 +12,36 @@ const computerPlay = () => {
     else return "Scissors";
 }
 
+const formatInput = input => {
+    return input.slice(0, 1).toUpperCase() + input.slice(1).toLowerCase();
+}
+
 const playRound = (playerSelection, computerSelection) => {
-    if (playerSelection = "Rock") {
+    // console.log(`${playerSelection}, ${computerSelection}`);
+    if (playerSelection === "Rock") {
         switch(computerSelection) {
             case "Rock" : return "Tie! Double rock!";
             case "Paper" : return "Loss! Their paper beat your rock!";
             case "Scissors" : return "Win! Your rock beat their paper!";
         }
     }
+    else if (playerSelection === "Paper") {
+        switch(computerSelection) {
+            case "Rock" : return "Win! Your paper beat their rock!";
+            case "Paper": return "Tie! Double paper!";
+            case "Scissors" : return "Loss! Their scissors beat your paper!";
+        }
+    }
+    else {
+        switch(computerSelection) {
+            case "Rock" : return "Loss! Their rock beats your scissors!";
+            case "Paper" : return "Win! Your scissors beats their paper!";
+            case "Scissors" : return "Tie! Double scissors!";
+        }
+    }
 }
 
-console.log(playRound("Rock", "Rock"));
+console.log(playRound(computerPlay(), computerPlay()));
 
 // let num1 = computerPlay();
 // console.log(num1);
